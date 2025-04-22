756d7e87b8d74437b50c0961b0a12e93b5ed96c4
SHenders2424 <s.henderson2424@gmail.com>
2025-04-22 15:11:27 -0500

    Created player class

--- SP2025_CS3330_HW4_G0/src/hw4/player/Player.java
+++ SP2025_CS3330_HW4_G0/src/hw4/player/Player.java
@@ -1,5 +1,67 @@
 package hw4.player;
 
-public class Player {
+import java.util.List;
+import hw4.maze.Cell;
+import hw4.maze.CellComponents;
+import hw4.maze.Row;
 
+public class Player {
+	private Row currentRow;
+	private Cell currentCell;
+	
+	public Player(Row currentRow, Cell currentCell) {
+		this.currentRow = currentRow;
+		this.currentCell = currentCell;
+	}
+	
+	public Row getCurrentRow() {
+		return currentRow;
+	}
+	
+	public Cell getCurrentCell() {
+		return currentCell;
+	}
+	
+	public boolean move(Movement direction, List<Row> grid) {
+		int rowIndex = grid.indexOf(currentRow);
+		int colIndex = currentRow.getCells().indexOf(currentCell);
+		
+		switch(direction) {
+			case UP:
+				if (currentCell.getUp() != CellComponents.WALL && rowIndex > 0) {
+					currentRow =grid.get(rowIndex - 1);
+					currentCell = currentRow.getCells().get(colIndex);
+					return true;
+				}
+				break;
+				
+			case DOWN:
+				if (currentCell.getDown() != CellComponents.WALL && rowIndex < grid.size()- 1) {
+					currentRow =grid.get(rowIndex + 1);
+					currentCell = currentRow.getCells().get(colIndex);
+					return true;
+				}
+				break;
+				
+			case LEFT:
+				if (currentCell.getLeft() != CellComponents.WALL && colIndex > 0) {
+					currentCell = currentRow.getCells().get(colIndex - 1);
+					return true;
+				}
+				break;
+				
+			case RIGHT:
+				if (currentCell.getRight() != CellComponents.WALL && colIndex < currentRow.getCells().size() - 1) {
+					currentCell = currentRow.getCells().get(colIndex + 1);
+					return true;
+				}
+				break;
+		}
+		return false;
+	}
+	
+	@Override
+	public String toString() {
+		return "Player's current cell:" + currentCell + ", current row:" + currentRow;
+	}
 }
